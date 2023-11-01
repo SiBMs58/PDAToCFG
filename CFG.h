5c01a5560e1fb119ff3aa3e19135f06cb53c23bb
Siebe Mees <siebe.mees@student.uantwerpen.be>
2023-11-01 18:16:59 +0100

    Print fix, in function of print statement of test code inginious

--- CFG.h
+++ CFG.h
@@ -5,9 +5,30 @@
 #ifndef PDATOCFG_CFG_H
 #define PDATOCFG_CFG_H
 
+#include <string>
+#include <vector>
+
+using namespace std;
 
 class CFG {
+private:
+    vector<string> V; // Set of variables
+    vector<string> T; // Set of terminals
+    string S; // Start symbol
+    vector<pair<string, vector<string>>> P; // Productions
 
+public:
+    CFG();
+
+    //CFG(const string &filename);
+
+    void print();
+
+    // Setters
+    void setV(const vector<string> &v);
+    void setT(const vector<string> &t);
+    void setS(const string &s);
+    void setP(const vector<pair<string, vector<string>>> &p);
 };
 
 
